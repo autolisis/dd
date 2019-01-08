819e8e1e512cef8ff82907b9e96e3672419fdb63
reisub0 <reisub0@gmail.com>
2019-01-08 10:50:54 +0530

    Added Assignment 1, lab 1
    Signed-off-by: reisub0 <reisub0@gmail.com>

---
+++ lab/1/5-ckernel/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/g/dd/lab/1/5-ckernel/hello.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/g/dd/lab/1/5-ckernel/hello.ko /home/g/dd/lab/1/5-ckernel/hello.o /home/g/dd/lab/1/5-ckernel/hello.mod.o ;  true
---
+++ lab/1/5-ckernel/.tmp_versions/hello.mod
@@ -0,0 +1,3 @@
+/home/g/dd/lab/1/5-ckernel/hello.ko
+/home/g/dd/lab/1/5-ckernel/hello.o
+
